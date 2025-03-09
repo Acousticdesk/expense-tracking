e87b0648e7c2aa6e163dca53c760b571e440df8b
Andrii Kycha <acousticdesk@gmail.com>
2025-03-08 22:58:08 -0700

    Added the AddTransactionCategoryPicker component

---
+++ client/src/components/AddTransactionCategoryPicker.tsx
@@ -0,0 +1,61 @@
+import {
+  Category,
+  fetchCategories,
+  getCategoriesFromFetchCategoriesResponse,
+  getCategoryId,
+  getCategoryTitle,
+} from "@/lib/services/categories.service";
+import { cn } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import { CategoryColorTile } from "./CategoryColorsSelect";
+
+interface AddTransactionCategoryPickerProps {
+  value: Category["id"];
+  onChange: (value: Category["id"]) => void;
+}
+
+export function AddTransactionCategoryPicker({
+  value,
+  onChange,
+}: AddTransactionCategoryPickerProps) {
+  // todo akicha: this should be a separate hook to persist the queryKey
+  const { data: categoriesResponse, isLoading: isCategoriesLoading } = useQuery(
+    {
+      queryKey: ["categories"],
+      queryFn: fetchCategories,
+    },
+  );
+
+  if (isCategoriesLoading) {
+    return "Loading...";
+  }
+
+  if (!categoriesResponse) {
+    return null;
+  }
+
+  const categories =
+    getCategoriesFromFetchCategoriesResponse(categoriesResponse);
+
+  return (
+    <ul className="flex gap-4 overflow-x-auto">
+      {categories.map((category) => (
+        <li key={getCategoryId(category)} className="min-w-max">
+          <button
+            type="button"
+            className={cn("py-1 px-2 rounded border border-gray-300", {
+              "bg-gray-300": getCategoryId(category) === value,
+            })}
+            onClick={() => onChange(getCategoryId(category))}
+          >
+            <div className="flex items-center gap-x-2">
+              {/* todo akicha: add the actual color of the category */}
+              <CategoryColorTile />
+              <p>{getCategoryTitle(category)}</p>
+            </div>
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- client/src/screens/AddTransaction.tsx
+++ client/src/screens/AddTransaction.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { FormField, FormItem, FormLabel, FormControl, FormMessage, Form } from "@/components/ui/form";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  Form,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { postTransactions } from "@/lib/services/transactions.service";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -9,84 +16,109 @@ import { useForm } from "react-hook-form";
 import { useParams, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import { z } from "zod";
+import { AddTransactionCategoryPicker } from "@/components/AddTransactionCategoryPicker";
 
 const formSchema = z.object({
-    title: z.string().optional(),
-    amount: z.string(),
+  title: z.string().optional(),
+  amount: z.string(),
+  category_id: z.union([z.string(), z.number()]),
+});
+
+export function AddTransaction() {
+  const { categoryId } = useParams();
+  const navigate = useNavigate();
+
+  // todo akicha: this validation should be a part of the categories service
+  const _categoryId = isNaN(Number(categoryId))
+    ? undefined
+    : Number(categoryId);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
   });
-  
-  export function AddTransaction() {
-    const { categoryId } = useParams();
-    const navigate = useNavigate();
-  
-    // todo akicha: this validation should be a part of the categories service
-    const _categoryId = isNaN(Number(categoryId))
-      ? undefined
-      : Number(categoryId);
-  
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-    });
-  
-    async function _handleSubmit() {
-      if (!_categoryId) {
-        return;
-      }
-  
-      try {
-        await postTransactions(_categoryId, form.getValues());
-        navigate("/");
-      } catch (error) {
-        console.error(error);
-  
-        toast(
-          "Oops... Something went wrong while adding the transaction. Please try again later.",
-        );
-      }
+
+  async function _handleSubmit() {
+    const values = form.getValues();
+    const selectedCategoryId = _categoryId || values.category_id;
+
+    if (!selectedCategoryId) {
+      console.log(
+        "No category selected for the transaction. Aborting adding the transaction.",
+      );
+      return;
     }
-  
-    return (
-      <Layout>
-        <Container className="flex-auto">
-          <div className="py-2">
-            <p className="text-xl">Add Transaction</p>
-  
-            <Form {...form}>
-              <form
-                className="flex flex-col gap-y-2 mt-2"
-                onSubmit={form.handleSubmit(_handleSubmit)}
-              >
-                <FormField
-                  name="title"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Title</FormLabel>
-                      <FormControl>
-                        <Input {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-  
-                <FormField
-                  name="amount"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Amount</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="tel" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-  
-                <Button type="submit">Add Transaction</Button>
-              </form>
-            </Form>
-          </div>
-        </Container>
-      </Layout>
-    );
+
+    try {
+      await postTransactions(selectedCategoryId, values);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+
+      toast(
+        "Oops... Something went wrong while adding the transaction. Please try again later.",
+      );
+    }
+
+    toast("Transaction added successfully.");
+    form.reset();
   }
+
+  return (
+    <Layout>
+      <Container className="flex-auto">
+        <div className="py-2">
+          <p className="text-xl">Add Transaction</p>
+
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-y-2 mt-2"
+              onSubmit={form.handleSubmit(_handleSubmit)}
+            >
+              <FormField
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Amount</FormLabel>
+                    <FormControl>
+                      <Input {...field} value={field.value || ""} type="tel" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                name="category_id"
+                defaultValue={_categoryId}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Category</FormLabel>
+                    <FormControl>
+                      <AddTransactionCategoryPicker {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit">Add Transaction</Button>
+            </form>
+          </Form>
+        </div>
+      </Container>
+    </Layout>
+  );
+}
